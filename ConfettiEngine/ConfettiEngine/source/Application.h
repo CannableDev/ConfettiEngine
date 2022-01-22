fe415472e285394bbe84e515eef49cb85349991f
Harrison Lorraway <cannabledev@gmail.com>
2022-01-22 21:39:06 +1100

    Replacing SDL with GLFW due to unnecessary complications Adding Vulkan libraries to solution

---
+++ ConfettiEngine/ConfettiEngine/source/Application.h
@@ -0,0 +1,19 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class Application {
+public:
+
+
+private:
+
+
+};
+
+#endif
--- ConfettiEngine/ConfettiEngine/source/ConfettiEngine.cpp
+++ ConfettiEngine/ConfettiEngine/source/ConfettiEngine.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/string_cast.hpp>
 
-#include <sdl/SDL.h>
-
 using namespace glm;
-
-SDL_Window* window;
-SDL_GLContext glContext;
-SDL_Event event;
-
-std::string mainWindowTitle;
-std::string displayDetails;
-
-bool quitApp = false;
-bool fullScreen = true;
-int windowWidth = 800;
-int windowHeight = 600;
-int screenWidth = 0;
-int screenHeight = 0;
 
 int main()
 {
+    /* TESTING DEMO 
     std::cout << "Hello World!\n";
     vec3 potato(1, 2, 3);
 
     std::cout << glm::to_string(potato) << std::endl;
 
-    /// SDL ///
+    glfwInit();
 
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0) {
-        std::cerr << "APP:ERROR: SDL2 video subsystem couldn't be initialized. Error: " << SDL_GetError() << std::endl;
-        return -1;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
     }
 
-    if (fullScreen == true) {
-        window = SDL_CreateWindow(mainWindowTitle.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
-    }
-    else {
-        window = SDL_CreateWindow(mainWindowTitle.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
-    }
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    */
+
 
     return EXIT_SUCCESS;
 }
